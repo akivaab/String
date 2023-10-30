f207e4b6d74fb9c88e38fff5d8c9fc14e6a6d14f
akivaab <amabramo18@gmail.com>
2023-10-30 18:20:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// You can use JavaScript to handle the tile falling logic and user interactions.
+
+// Here's a basic example of generating a random tile and making it fall:
+function createRandomTile() {
+    const tile = document.createElement('div');
+    tile.classList.add('tile');
+    // Customize the appearance of the tile as needed
+    return tile;
+}
+
+function startGame() {
+    const grid = document.querySelector('.grid');
+
+    // Create and append a random tile to the top row
+    const randomTile = createRandomTile();
+    grid.firstElementChild.appendChild(randomTile);
+
+    // Move the tile down and handle collisions
+    function moveTileDown() {
+        // Logic for moving the tile down
+    }
+
+    // Handle user interactions, e.g., moving the tile left or right
+
+    // Start a game loop (e.g., using requestAnimationFrame) to move the tile down at a certain speed
+    function gameLoop() {
+        moveTileDown();
+        requestAnimationFrame(gameLoop);
+    }
+
+    gameLoop();
+}
+
+startGame();
+
+console.log(visualViewport.height);
