520be34c2bbc1c58c14ea788dce6fda6034412e9
akivaab <amabramo18@gmail.com>
2023-12-26 18:50:27 +0200

    audio simplification (but still broken)

--- audio.js
+++ audio.js
@@ -1,44 +1,27 @@
-const /** @type {HTMLAudioElement} */ music60_1 = document.getElementById('music60-1');
-const /** @type {HTMLAudioElement} */ music60_2 = document.getElementById('music60-2');
-const /** @type {HTMLAudioElement} */ music80_1 = document.getElementById('music80-1');
-const /** @type {HTMLAudioElement} */ music80_2 = document.getElementById('music80-2');
+const /** @type {HTMLAudioElement} */ music60 = document.getElementById('music60');
+const /** @type {HTMLAudioElement} */ music80 = document.getElementById('music80');
+let currentMusic = music60;
 
 export function toggleMute() {
-    music60_1.muted = !music60_1.muted;
-    music60_2.muted = !music60_2.muted;
-    music80_1.muted = !music80_1.muted;
-    music80_2.muted = !music80_2.muted;
+    music60.muted = !music60.muted;
+    music80.muted = !music80.muted;
 }
 
 export function playAudio() {
-    music60_1.play();
+    music60.play();
 }
 
 export function loopAudioCheck() {
-    if (music60_1.currentTime >= 60) music60_2.play();
-    else if (music60_2.currentTime >= 60) music60_1.play();
-    else if (music80_1.currentTime >= 45) music80_2.play();
-    else if (music80_2.currentTime >= 45) music80_1.play();
+    if (music60.currentTime >= 60) music60.currentTime = 0;
+    else if (music80.currentTime >= 45) music80.currentTime = 0;
 }
 
 export function speedUpAudio() {
-    if (!music60_1.paused) {
-        switchFromTo(music60_1, music80_1, 0.75);
-    }
-    else if (!music60_2.paused) {
-        switchFromTo(music60_2, music80_2, 0.75);
-        console.log(music60_2.paused);
-        console.log(!music80_2.paused);
-    }
+    if (currentMusic == music60) switchFromTo(music60, music80, 0.75);
 }
 
 export function slowDownAudio() {
-    if (!music80_1.paused) {
-        switchFromTo(music80_1, music60_1, 4 / 3)
-    }
-    else if (!music80_2.paused) {
-        switchFromTo(music80_2, music60_2, 4 / 3)
-    }
+    if (currentMusic == music80) switchFromTo(music80, music60, 4 / 3);
 }
 
 function switchFromTo(curMusic, newMusic, beatRatio) {
